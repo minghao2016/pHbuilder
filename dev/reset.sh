054cc012847c37c1bbb705982b97927b142f4815
anton <anton@tcbl13.scilifelab.se>
2020-09-25 19:40:57 +0200

    small update

--- dev/reset.sh
+++ dev/reset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf \_\_py*
-rm -f posre.itp topol.top mdout.mdp 1cvo_*.pdb buffer.* charmm36* *.mdp *.tpr
+rm -rf \_\_py* charmm*
+rm -f posre.itp topol.top mdout.mdp 1cvo_*.pdb buffer.* charmm36* *.mdp *.tpr *.log
 rm -f \#*\#
 rm -f *.ndx
