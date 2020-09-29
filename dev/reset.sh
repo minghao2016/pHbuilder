568bb6a1f32f26a438bfbf0828f3395c0107a908
anton <anton@tcbl13.scilifelab.se>
2020-09-29 18:18:08 +0200

    updated

--- dev/reset.sh
+++ dev/reset.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 rm -rf \_\_py* charmm* buffer.pdb
-rm -f *.itp *.top *.mdp 1cvo_*.pdb *.tpr *.log *.ndx *.edr *.trr *.cpt *.dat
+rm -f 2khm_*.pdb 1cvo_*.pdb
+rm -f *.itp *.top *.mdp *.tpr *.log *.ndx *.edr *.trr *.cpt *.dat
 rm -f \#*\#
