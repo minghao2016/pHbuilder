84aaec24237a3db8f902889e82853c687142b40f
anton <anton@tcbl13.scilifelab.se>
2020-09-30 17:51:58 +0200

    updated

--- dev/run.sh
+++ dev/run.sh
@@ -4,10 +4,10 @@
 source /usr/local/gromacs_dev/bin/GMXRC
 
 # Compile
-gmx grompp -f MD.mdp -c 2khm_NPT.pdb -p topol.top -n index.ndx -o MD.tpr
+gmx grompp -f MD.mdp -c 1cvo_NPT.pdb -p topol.top -n index.ndx -o MD.tpr
 
 # Run
-gmx mdrun -v -s MD.tpr -o MD.trr -c 2khm_MD.pdb -g MD.log -e MD.edr
+gmx mdrun -v -s MD.tpr -o MD.trr -c 1cvo_MD.pdb -g MD.log -e MD.edr
 
 # Load normal version
 source /usr/local/gromacs/bin/GMXRC
