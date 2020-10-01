a852d15ba13ec5f81bf6634dc5cc7da6092f0478
anton <anton@tcbl13.scilifelab.se>
2020-10-01 16:52:34 +0200

    updated

--- dev/run.sh
+++ dev/run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Load constant-pH version of Gromacs
+# NOORA ########################################################################
+
 source /usr/local/gromacs_dev/bin/GMXRC
 
-# Compile
 gmx grompp -f MD.mdp -c 1cvo_NPT.pdb -p topol.top -n index.ndx -o MD.tpr
 
-# Run
 gmx mdrun -v -s MD.tpr -o MD.trr -c 1cvo_MD.pdb -g MD.log -e MD.edr
+
+# PAUL #########################################################################
+
+# source /usr/local/gromacs_paul/bin/GMXRC
+
+# gmx grompp -f MD.mdp -c 1cvo_NPT.pdb -p topol.top -n index.ndx -o MD.tpr
+
+# gmx mdrun -v -s MD.tpr -o MD.trr -c 1cvo_MD.pdb -g MD.log -e MD.edr -cpHMD constant_ph_input.dat
+
+################################################################################
 
 # Load normal version
 source /usr/local/gromacs/bin/GMXRC
