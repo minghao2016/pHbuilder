c7674bc281b5f8c0cdb91b81f37de02327df27e6
anton <anton@tcbl13.scilifelab.se>
2020-10-08 12:41:47 +0200

    added continue commands

--- dev/run.sh
+++ dev/run.sh
@@ -4,9 +4,9 @@
 
 source /usr/local/gromacs_dev/bin/GMXRC
 
-gmx grompp -f MD.mdp -c 1cvo_NPT.pdb -p topol.top -n index.ndx -o MD.tpr
+# gmx grompp -f MD.mdp -c 1cvo_NPT.pdb -p topol.top -n index.ndx -o MD.tpr -r 1cvo_NPT.pdb
 
-gmx mdrun -v -s MD.tpr -o MD.trr -c 1cvo_MD.pdb -g MD.log -e MD.edr
+# gmx mdrun -v -s MD.tpr -o MD.trr -c 1cvo_MD.pdb -g MD.log -e MD.edr
 
 # PAUL #########################################################################
 
@@ -17,6 +17,10 @@ gmx mdrun -v -s MD.tpr -o MD.trr -c 1cvo_MD.pdb -g MD.log -e MD.edr
 # gmx mdrun -v -s MD.tpr -o MD.trr -c 1cvo_MD.pdb -g MD.log -e MD.edr -cpHMD constant_ph_input.dat
 
 ################################################################################
+
+# CONTINUE
+# gmx convert-tpr -s MD.tpr -o MD.tpr -extend 2000
+# gmx mdrun -v -cpi state.cpt -append -s MD.tpr -o MD.trr -c 1cvo_MD.pdb -g MD.log -e MD.edr
 
 # Load normal version
 source /usr/local/gromacs/bin/GMXRC
