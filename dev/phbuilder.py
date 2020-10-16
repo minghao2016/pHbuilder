e1204f1421bed46ec4f6f49067f468ee61afb2b6
anton <anton@tcbl13.scilifelab.se>
2020-10-16 17:13:47 +0200

    some more cleanup

--- dev/phbuilder.py
+++ dev/phbuilder.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 import os, sys
-from lib import sim, backupFile
+from lib import sim
 
 # Set up environment/external files.
 gromPath = "/home/anton/GIT/phbuilder/grom"
 os.system("cp {0}/buffer.itp {0}/buffer.pdb {0}/IONS.mdp ./".format(gromPath))
-backupFile("builder.log")
 
 ################################################################################
 
@@ -21,8 +20,7 @@ sim.protein_add_buffer(minSep=1.5)
 sim.protein_add_water()
 sim.protein_add_ions()
 
-# GENERATE INDEX FILE
-
+# GROUPS
 group_PROTEIN     = ['ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY',
                      'HIS', 'ILE', 'LEU', 'LYS', 'MET', 'PRO', 'SER', 'THR',
                      'VAL', 'PHE', 'TYR', 'TRP']
@@ -32,6 +30,7 @@ group_IONS        = [' NA', ' CL']
 group_NON_PROTEIN = group_BUFFER + group_WATER + group_IONS
 group_SYSTEM      = group_PROTEIN + group_NON_PROTEIN
 
+# GENERATE INDEX FILE
 sim.generate_index("SYSTEM"     , group_SYSTEM     )
 sim.generate_index("PROTEIN"    , group_PROTEIN    )
 sim.generate_index("BUFFER"     , group_BUFFER     )
@@ -40,24 +39,20 @@ sim.generate_index("IONS"       , group_IONS       )
 sim.generate_index("NON_PROTEIN", group_NON_PROTEIN)
 
 # GENERATE .MDP FILES
-
-sim.generate_mdp('EM',  dt=0.01,  nsteps=10000,  output=0,    tgroups=[['SYSTEM', 0.5, 300]])
-sim.generate_mdp('NVT', dt=0.002, nsteps=25000,  output=0,    tgroups=[['SYSTEM', 0.5, 300]])
-sim.generate_mdp('NPT', dt=0.002, nsteps=25000,  output=0,    tgroups=[['SYSTEM', 0.5, 300]])
-sim.generate_mdp('MD',  dt=0.002, nsteps=5000,   output=1,    tgroups=[['SYSTEM', 0.5, 300]])
+sim.generate_mdp('EM')
+sim.generate_mdp('NVT')
+sim.generate_mdp('NPT')
+sim.generate_mdp('MD',  nsteps=5000,  output=1)
 
 # GENERATE CONSTANT-PH .DAT FILE
-
-sim.generate_phdata(4.5)
+sim.generate_phdata(pH=4.5, lambdaM=10, nstOut=1, barrierE=7.5)
 
 # WRITE BASH SCRIPTS
-
 sim.write_run("/usr/local/gromacs", "/usr/local/gromacs_dev")
-sim.write_jobscript("test", 1, 1, 32, "tcb")
+sim.write_jobscript(jobName="test", time=1, nodes=1, ntasks=32, queue="tcb")
 sim.write_reset()
 
 # EQUILIBRATE
-
 sim.energy_minimize()
 sim.energy_tcouple()
 sim.energy_pcouple()
