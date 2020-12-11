4903ef2ff35f0d0419f3595f8af0af628ab37bc3
Anton Jansen <antonjansen96@gmail.com>
2020-12-11 15:57:23 +0100

    cleanup

--- dev/phbuilder.py
+++ dev/phbuilder.py
@@ -11,9 +11,9 @@ os.system("cp {0}/buffer.itp {0}/buffer.pdb {0}/IONS.mdp ./".format(gromPath))
 
 sim = sim()
 sim.setconstantpH(True, restrain=True)
-sim.processpdb("1cvo.pdb")
+sim.processpdb("3lr2.pdb")
 
-sim.protein_add_forcefield("charmm36-mar2019", "tip3p")
+sim.protein_add_forcefield("charmm36-mar2019", "tip3p", neutralTermini=True)
 sim.protein_add_box(boxSizeMargin=1.0)
 sim.protein_add_buffer(minSep=1.5)
 sim.protein_add_water()
@@ -24,12 +24,13 @@ sim.generate_index()
 sim.generate_mdp('EM')
 sim.generate_mdp('NVT')
 sim.generate_mdp('NPT')
-sim.generate_mdp('MD', nsteps=2500000, nstxout=1000)
+sim.generate_mdp('MD', nsteps=100000, nstxout=1000)
 
 # sim.generate_phdata(pH=4.5, lambdaM=5.0, nstOut=1, barrierE=5.0)
 sim.generate_phdata_legacy(pH=4.25, lambdaM=5.0, nstOut=1, barrierE=5.0)
 
-sim.write_run("/usr/local/gromacs_test", mode='gpu')
+sim.write_jobscript('test', 1, 1, 32, 'lindahl')
+sim.write_run("/usr/local/gromacs_test2", mode='gpu')
 sim.write_reset()
 
 sim.energy_minimize()
