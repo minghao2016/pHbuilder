cd8514798fdf4b0649143894c628e9491cfa0d4d
Anton Jansen <antonjansen96@gmail.com>
2021-04-21 19:43:17 +0200

    add small analysis example

---
+++ examples/01_build_tripeptide/plotresult.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+
+import phbuilder
+
+phbuilder.analyze.plotlambda()
+phbuilder.analyze.plothistogram('lambda_1.dat')
---
+++ examples/03_calibrate/replot.py
@@ -0,0 +1,5 @@
+#!/bin/python3
+
+import phbuilder
+
+phbuilder.analyze.fitCalibration(order=5, compare=[26.238, -556.92, -106.76, 230.33, -155.89, -24.960])
